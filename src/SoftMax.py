93fabb3b14c6205304b5f74d40eb27f1e2fdf555
KKStarter <zhanggc.kelvin@gmail.com>
2014-04-21 23:25:16 -0400

    Softmax modified

--- src/SoftMax.py
+++ src/SoftMax.py
@@ -5,10 +5,11 @@ from scipy.linalg import norm
 from scipy.optimize import fmin_l_bfgs_b
 from softmax_cost import *
 import numpy.random as rnd
+import numpy
 class SoftMax:
     """
     Implements a class for computing the softmax classifier.
- 
+
     Example usage::
 
         # load data
@@ -17,29 +18,29 @@ class SoftMax:
         data2 = np.transpose(mat_file['data2']) # data from distribution 1
         data3 = np.transpose(mat_file['data3']) # data from distribution 2
 
-        # set up kernel  
+        # set up kernel
         k_name = "gaussian"
         k_params = array( [3] ) # numpy array
-        k = KernelType(k_name, k_params)        
- 
+        k = KernelType(k_name, k_params)
+
         # initialize MeanMap
         mm_obj = MeanMap(k)
         mm_obj.process(data) # build map
 
         # compute maximum mean discrepancy between data1 and data2
-        dist1 = mm_obj.mmd(data2) 
-        dist2 = mm_obj.mmd(data3) 
+        dist1 = mm_obj.mmd(data2)
+        dist2 = mm_obj.mmd(data3)
         print "The MMD between data1 and data2 is " + str(dist1)
         print "The MMD between data1 and data3 is " + str(dist2)
 
-    :param k_type: Kernel 
+    :param k_type: Kernel
     :type k_type: Kernel object
     :return: `MeanMap object`
     :rtype:  MeanMap
     """
     def __init__(self, inputdim, numclasses, wdecayparm):
         """
-        Initialize default parameters. 
+        Initialize default parameters.
         """
         self.dim = inputdim
         self.nclasses = numclasses
@@ -48,56 +49,75 @@ class SoftMax:
 
     def cost(self, theta):
         """
-        This function computes the cost associated to the softmax classifier. 
+        This function computes the cost associated to the softmax classifier.
 
-        :param data: data matrix :math:`D_1\in\mathbb{R}^{d \\times n}`, where 
-                     :math:`d` is the dimensionality and 
+        :param data: data matrix :math:`D_1\in\mathbb{R}^{d \\times n}`, where
+                     :math:`d` is the dimensionality and
                      :math:`n` is the number of training samples
         :type data: numpy array
-        """        
-        # unroll parameters from theta 
-        theta = reshape(theta,(self.nclasses,self.dim))
+        """
+        # unroll parameters from theta
+        theta = reshape(theta,(self.dim,self.nclasses))
+        theta=theta.T
         nsamp = self.data.shape[1]
 
         # generate ground truth matrix
         onevals = squeeze(ones((1,nsamp)))
-        rows = squeeze(self.labels)
+        rows = squeeze(self.labels)-1
         cols = arange(nsamp)
         ground_truth = csr_matrix((onevals,(rows,cols))).todense()
 
         # compute hypothesis; use some in-place computations
         theta_dot_prod = dot(theta,self.data)
-        theta_dot_prod = theta_dot_prod - npmax(theta_dot_prod)
-        soft_theta = npexp(theta_dot_prod)        
+        theta_dot_prod = theta_dot_prod - numpy.amax(theta_dot_prod, axis=0)
+        soft_theta = npexp(theta_dot_prod)
         soft_theta_sum = npsum(soft_theta,axis=0)
         soft_theta_sum = tile(soft_theta_sum,(self.nclasses,1))
         hyp = soft_theta/soft_theta_sum
-       
+
         # compute cost
-        log_hyp = nplog(hyp)      
+        log_hyp = nplog(hyp)
         temp = array(multiply(ground_truth,log_hyp))
-        temp = npsum(npsum(temp,axis=1),axis=0)        
+        temp = npsum(npsum(temp,axis=1),axis=0)
         cost = (-1.0/nsamp)*temp + 0.5*self.wdecay*pow(norm(theta,'fro'),2)
         thetagrad = (-1.0/nsamp)*dot(ground_truth-hyp,transpose(self.data)) + self.wdecay*theta
 
         thetagrad = thetagrad.flatten(1)
         return cost, thetagrad
 
-    def train(self,data,labels):
+    def train(self,data,labels, maxIter):
         """
-        This function trains the softmax classifier. 
+        This function trains the softmax classifier.
 
-        :param data: data matrix :math:`D_1\in\mathbb{R}^{d \\times n}`, where 
-                     :math:`d` is the dimensionality and 
+        :param data: data matrix :math:`D_1\in\mathbb{R}^{d \\times n}`, where
+                     :math:`d` is the dimensionality and
                      :math:`n` is the number of training samples
         :type data: numpy array
-        """        
-        res = fmin_l_bfgs_b(softmax_cost, self.theta, fprime=softmax_grad, 
-              args=(self.nclasses, self.dim, self.wdecay, data, labels), approx_grad=0, bounds=None, maxfun=400)
-        self.theta = reshape(res[0],(self.nclasses,self.dim)) # store returned result 
-        print res[0]
-        print res[1]
-        print res[2]
+        """
+        xx, ff, dd = fmin_l_bfgs_b(softmax_cost, self.theta, fprime=softmax_grad, iprint=1,
+              args=(self.nclasses, self.dim, self.wdecay, data, labels), approx_grad=0, bounds=None, maxfun=maxIter)
+
+#          xx, ff, dd = fmin_l_bfgs_b(self.cost, self.theta, fprime=softmax_grad,
+#               args=(self.nclasses, self.dim, self.wdecay, data, labels), approx_grad=0, bounds=None, maxfun=400)
+
+        self.theta = reshape(xx,(self.dim,self.nclasses))
+        self.theta = self.theta.T # store returned result
+#         print "XX"
+#         print xx
+#
+#         print "self.theta"
+#         print self.theta
+#
+#         print "ff"
+#         print ff
+#
+#         print "dd"
+#         print dd
+
+        return xx
+
+
+
 
 
 
--- src/softmax_cost.py
+++ src/softmax_cost.py
@@ -1,58 +1,70 @@
-# returns softmax cost associated to a given theta parameter 
-# data assumed to be in column form 
+# returns softmax cost associated to a given theta parameter
+# data assumed to be in column form
 from numpy import reshape, ones, squeeze, asarray, arange, dot, tile, multiply, array, transpose
 from numpy import max as npmax, exp as npexp, sum as npsum, log as nplog
+import numpy
 from scipy.sparse import csr_matrix
 from scipy.linalg import norm
-from scipy.optimize import minimize
+# from scipy.optimize import minimize
+from scipy.optimize import *
 import numpy.random as rnd
+
+import matplotlib.pyplot as plt
+
+
 def softmax_cost( theta, nclasses, dim, wdecay, data, labels ):
-    # unroll parameters from theta 
-    theta = reshape(theta,(nclasses,dim))
+    # unroll parameters from theta
+    theta = reshape(theta,(dim, nclasses)) # This was wrong
+    theta= theta.T
     nsamp = data.shape[1]
-    #print theta
 
     # generate ground truth matrix
     onevals = squeeze(ones((1,nsamp)))
-    rows = squeeze(labels)
+    rows = squeeze(labels)-1 # This was wrong
     cols = arange(nsamp)
     ground_truth = csr_matrix((onevals,(rows,cols))).todense()
 
+
     # compute hypothesis; use some in-place computations
     theta_dot_prod = dot(theta,data)
-    theta_dot_prod = theta_dot_prod - npmax(theta_dot_prod)
-    soft_theta = npexp(theta_dot_prod)        
+    theta_dot_prod = theta_dot_prod - numpy.amax(theta_dot_prod, axis=0) # This was wrong
+    soft_theta = npexp(theta_dot_prod)
     soft_theta_sum = npsum(soft_theta,axis=0)
     soft_theta_sum = tile(soft_theta_sum,(nclasses,1))
     hyp = soft_theta/soft_theta_sum
-       
+
+
     # compute cost
-    log_hyp = nplog(hyp)      
+    log_hyp = nplog(hyp)
     temp = array(multiply(ground_truth,log_hyp))
-    temp = npsum(npsum(temp,axis=1),axis=0)        
+    temp = npsum(npsum(temp,axis=1),axis=0)
     cost = (-1.0/nsamp)*temp + 0.5*wdecay*pow(norm(theta,'fro'),2)
     return cost
 
 def softmax_grad( theta, nclasses, dim, wdecay, data, labels ):
-    # unroll parameters from theta 
-    theta = reshape(theta,(nclasses,dim))
+    # unroll parameters from theta
+    theta = reshape(theta,(dim, nclasses)) # Do this
+    theta= theta.T
     nsamp = data.shape[1]
-    #print theta
 
     # generate ground truth matrix
     onevals = squeeze(ones((1,nsamp)))
-    rows = squeeze(labels)
+    rows = squeeze(labels)-1 # Here should -1 to align zero-indexing
     cols = arange(nsamp)
+
     ground_truth = csr_matrix((onevals,(rows,cols))).todense()
+#     plt.imshow(ground_truth,interpolation='nearest')
+#     plt.draw()
+#     print ground_truth
 
     # compute hypothesis; use some in-place computations
     theta_dot_prod = dot(theta,data)
-    theta_dot_prod = theta_dot_prod - npmax(theta_dot_prod)
-    soft_theta = npexp(theta_dot_prod)        
+    theta_dot_prod = theta_dot_prod - numpy.amax(theta_dot_prod, axis=0) # This was wrong
+    soft_theta = npexp(theta_dot_prod)
     soft_theta_sum = npsum(soft_theta,axis=0)
     soft_theta_sum = tile(soft_theta_sum,(nclasses,1))
     hyp = soft_theta/soft_theta_sum
-       
+
     # compute gradient
     thetagrad = (-1.0/nsamp)*dot(ground_truth-hyp,transpose(data)) + wdecay*theta
 
@@ -60,4 +72,21 @@ def softmax_grad( theta, nclasses, dim, wdecay, data, labels ):
     thetagrad = thetagrad.flatten(1)
     return thetagrad
 
+def softmax_predict( theta, nclasses, dim,  data ):
+    # unroll parameters from theta
+    theta = reshape(theta,(dim, nclasses)) # Do this
+    theta= theta.T
+
+    # compute hypothesis; use some in-place computations
+    theta_dot_prod = dot(theta,data)
+    theta_dot_prod = theta_dot_prod - numpy.amax(theta_dot_prod, axis=0) # This was wrong
+    soft_theta = npexp(theta_dot_prod)
+    soft_theta_sum = npsum(soft_theta,axis=0)
+    soft_theta_sum = tile(soft_theta_sum,(nclasses,1))
+    hyp = soft_theta/soft_theta_sum
+    print "hyp.shape"
+    print hyp.shape
+    pred=numpy.argmax(hyp, axis=0)
+
+    return numpy.asarray(pred)
 
